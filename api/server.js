38ee6310ed66edfb95bc4abe16d276a414661f22
Akshay Sakat <aakshaysakat@gmail.com>
2021-01-09 15:30:24 +0530

    added favicon

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 let express = require('express');
 
 const apiRoutes = require('./Routes/apiroutes');
+const favicon = require('serve-favicon');
 
 const app = express();
 app.use(express.json());
-
 const cors = require('cors');
-
+app.use(favicon(path.join(__dirname,'public', 'favicon.ico')));
 app.use('/api', apiRoutes);
 
   module.exports = app
